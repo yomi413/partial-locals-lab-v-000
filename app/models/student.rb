7bda4c035c8393497d51fe4400d86c870eed526a
Yomaira Escano <yjescano@gmail.com>
2018-03-29 14:53:51 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,16 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :birthday, presence: true
+
+  def self.search(arg)
+    if !arg.empty?
+      where('NAME like ?', "%#{arg}%")
+    else
+      self.all
+    end
+  end
+
 end
